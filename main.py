834fea23d0b5e06e202a91ab6191c1bd5d76aa47
zaomqa <zz7035952@gmail.com>
2026-07-01 14:18:04 +0300

    added dynamic endpoints

--- main.py
+++ main.py
@@ -22,3 +22,24 @@ def read_greet():
     return {
     "message": "Welcome to my API!"
 }
+
+@app.get("/greet/{name}")
+def read_greet_name(name: str):
+    return {
+    "message": f"Hello, {name}"
+    }
+
+@app.get("/square/{number}")
+def read_square(number: int):
+    return {
+        "number": number,
+        "square": number ** 2
+    }
+
+@app.get("/add/{num1}/{num2}")
+def read_add(num1 : int, num2: int):
+    return {
+        "num1": num1,
+        "num2": num2,
+        "sum": num1 + num2
+    }
